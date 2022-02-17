90c91bc1f0f117339b1a25c1c8bb8ae2129f14fa
Travis <tsahadeo@mac.com>
2022-02-17 12:40:38 -0500

    commit tree c5a2c0fe53f50fa589b4e8756851d0f5d9f4510c

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+// const formData = {
+//     name: "Steve",
+//     email: "steve@steve.com"
+// }
+
+function submitData(firstName, email) {
+    const testObject = {
+        method: "POST",
+        headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: firstName,
+            email: email,
+        })
+    };
+return fetch("http://localhost:3000/users", testObject)
+.then(response => response.json())
+.then(body => addNewContact(body.id))
+.catch(someError)
+}
+
+function addNewContact(newID){
+    document.getElementsByTagName("body")[0].innerHTML = newID;
+    //document.body.append(message)
+}
+
+function someError(error){
+    document.body.innerHTML = "Unauthorized Access"
+}
+
